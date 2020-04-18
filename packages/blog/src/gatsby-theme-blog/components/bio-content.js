b3755c977bbd9c104770050f1857d607c51d9fb6
nicopresto <nicopresto@gmail.com>
2020-04-18 00:19:18 -0700

    Uses component shadowing to change bio byline

---
+++ packages/blog/src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+export default () => (
+  <Fragment>
+    Words by <Styled.a href="http://nicopresto.com/">Nicopresto</Styled.a>.
+    <br />
+    This is me
+  </Fragment>
+)
